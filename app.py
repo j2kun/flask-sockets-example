0ea1aea6a7d31c73b574984ca9d3b7bbd66c6bd5
Jeremy Kun <jeremy@21.co>
2016-10-08 20:34:57 -0700

    add chat app socketio

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask
+from flask import render_template
+from flask_socketio import SocketIO
+from flask_socketio import emit
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+
+@socketio.on('chat message', namespace='/chat')
+def handle_chat_message(json):
+    print('received message: ' + str(json))
+    emit('chat message', str(json), broadcast=True)
+
+
+@socketio.on('connect')  # global namespace
+def handle_connect():
+    print('Client connected')
+
+
+@socketio.on('connect', namespace='/chat')
+def handle_chat_connect():
+    print('Client connected to chat namespace')
+    emit('chat message', 'welcome!')
+
+
+@socketio.on('disconnect', namespace='/chat')
+def test_disconnect():
+    print('Client disconnected')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    socketio.run(app)
